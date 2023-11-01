acd97d130f2dc2bbbf63f27e290dba7582f33941
arcataroger <arcataroger@gmail.com>
2023-10-31 17:37:15 -0700

    does there need to be a loader for gets too?

--- app/routes/preview/start.js
+++ app/routes/preview/start.js
@@ -6,9 +6,21 @@ export const action = async ({ request, params }) => {
 
   session.set('preview', 'yes');
 
-  return redirect(params.redirect ?? '/', {
+  return redirect('/', {
     headers: {
       'Set-Cookie': await commitSession(session),
     },
   });
 };
+
+export const loader = async ({params}) => {
+  const session = await getSession(request.headers.get('Cookie'));
+
+  session.set('preview', 'yes');
+
+  return redirect(params.redirect ?? '/', {
+    headers: {
+      'Set-Cookie': await commitSession(session),
+    },
+  });
+}
